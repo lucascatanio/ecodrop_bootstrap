5a68db6d7052657031e4c63cbb3923c0a9657f4a
lucascatanio <lucascatanio@gmail.com>
2024-06-20 01:34:02 -0300

    segundo commit

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,41 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const boxes = document.querySelectorAll('.box');
+
+    const handleScroll = () => {
+        boxes.forEach(box => {
+            const boxTop = box.getBoundingClientRect().top;
+            const triggerPoint = window.innerHeight - 100;
+
+            if (boxTop < triggerPoint) {
+                box.classList.add('visible');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Chama a função inicialmente para verificar a posição dos elementos
+});
+
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const sr = ScrollReveal({
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        delay: 200,
+        reset: true
+    });
+
+    sr.reveal('.reveal', {
+        beforeReveal: function (el) {
+            const typingText = el.querySelector('.typing-text');
+            if (typingText) {
+                typingText.style.visibility = 'visible';
+                typingText.style.animationPlayState = 'running';
+            }
+        }
+    });
+});
